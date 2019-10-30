51aa3a243cf5711b8d17bde7bcb16de3cf197d75
William Wong <compulim@users.noreply.github.com>
2019-10-30 11:48:31 +0800

    Support IE11 (#4)

--- npmwebchat/src/index.js
+++ npmwebchat/src/index.js
@@ -1,3 +1,21 @@
+import 'core-js/features/array/find-index';
+import 'core-js/features/array/find';
+import 'core-js/features/array/includes';
+import 'core-js/features/array/iterator';
+import 'core-js/features/dom-collections';
+import 'core-js/features/math/sign';
+import 'core-js/features/number/is-finite';
+import 'core-js/features/object/assign';
+import 'core-js/features/object/values';
+import 'core-js/features/promise';
+import 'core-js/features/promise/finally';
+import 'core-js/features/string/starts-with';
+import 'core-js/features/symbol';
+
+import 'react-app-polyfill/ie11';
+import 'url-search-params-polyfill';
+import 'whatwg-fetch';
+
 import React from "react";
 import ReactDOM from "react-dom";
 import "./index.css";
