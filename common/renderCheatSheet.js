99e47df562e7c921754a32e78389ac3f4b03855e
William Wong <compulim@hotmail.com>
2019-12-30 04:00:54 -0800

    Cosmetics

---
+++ common/renderCheatSheet.js
@@ -0,0 +1,34 @@
+const VERSION_TABLE_TEMPLATE = `
+## Versions
+
+This version table is generated from \`<meta>\` tags.
+
+| Name | Version |
+| - | - |
+{CONTENT}
+`
+
+function buildVersionTable() {
+  return VERSION_TABLE_TEMPLATE.replace('{CONTENT}', [].map.call(
+    document.querySelectorAll('head meta[name^="bot"]'),
+    function (meta) {
+      return `| \`${meta.name}\` | \`${meta.content}\` |`;
+    }
+  ).join('\n'));
+}
+
+(async () => {
+  const container = document.createElement('div');
+  const content = document.createElement('div');
+  const res = await fetch('README.md');
+  const markdown = await res.text();
+  const footerText = buildVersionTable();
+
+  content.className = 'markdown';
+  content.innerHTML = window.markdownit().render(markdown + '\n\n' + footerText);
+
+  container.appendChild(content);
+  container.id = 'cheat-sheet';
+
+  document.body.appendChild(container);
+})().catch(err => console.error(err));
