eaa1b8fccd6cff15b7ed21bf04a03a9d91dcf166
William Wong <compulim@hotmail.com>
2020-03-05 14:53:37 -0800

    Add install scripts

---
+++ 01.create-react-app/install-drops.cmd
@@ -0,0 +1,5 @@
+call npm install
+call npm install ../drops/botframework-webchat-core-4.8.0.tgz
+call npm install ../drops/botframework-webchat-component-4.8.0.tgz
+call npm install ../drops/botframework-directlinespeech-sdk-4.8.0.tgz
+call npm install ../drops/botframework-webchat-4.8.0.tgz
