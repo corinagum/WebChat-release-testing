91d0ef7439c0293571b3410e7c979ff1a270b405
Corina <14900841+corinagum@users.noreply.github.com>
2021-02-26 18:42:10 -0800

    Update install-drops.cmd

--- 01.create-react-app/install-drops.cmd
+++ 01.create-react-app/install-drops.cmd
@@ -1,5 +1,6 @@
 call npm install
-call npm install ../drops/botframework-webchat-core-4.8.0.tgz
-call npm install ../drops/botframework-webchat-component-4.8.0.tgz
-call npm install ../drops/botframework-directlinespeech-sdk-4.8.0.tgz
-call npm install ../drops/botframework-webchat-4.8.0.tgz
+call npm install ../drops/botframework-webchat-core-4.11.0.tgz
+call npm install ../drops/botframework-webchat-api-4.11.0.tgz
+call npm install ../drops/botframework-webchat-component-4.11.0.tgz
+call npm install ../drops/botframework-directlinespeech-sdk-4.11.0.tgz
+call npm install ../drops/botframework-webchat-4.11.0.tgz
