a5472a6adb66b551175c4a2679fa10f7c95da3c0
William Wong <compulim@hotmail.com>
2024-10-09 17:57:14 -0700

    CJS

--- 07.webpack4/webpack.config.js
+++ 07.webpack4/webpack.config.js
@@ -1,13 +1,29 @@
 const { resolve } = require('path');
 const TerserPlugin = require('terser-webpack-plugin');
 
+const whitelistedPackages = [
+  'botframework-webchat-fluent-theme',
+  'htmlparser2',
+  'iter-fest',
+  'micromark-util-decode-numeric-character-reference',
+  'micromark-util-sanitize-uri',
+  'microsoft-cognitiveservices-speech-sdk',
+  'mime',
+  'valibot'
+];
+
 module.exports = {
+  amd: { jQuery: true },
   entry: './src/index.js',
   mode: 'production',
   module: {
     rules: [
       {
-        test: /\.(jsx?|mjs)$/,
+        test: /\.m?js$/,
+        include: [
+          resolve(__dirname, 'src'),
+          ...whitelistedPackages.map(packageName => resolve(__dirname, 'node_modules', packageName))
+        ],
         use: {
           loader: 'babel-loader',
           options: {
